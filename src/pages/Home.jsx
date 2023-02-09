578c0bbca58561708b746744c0ba6a367d9aa3be
Reb1324 <105323507+Reb1324@users.noreply.github.com>
2023-02-09 20:59:53 -0300

    Requisito 1

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { getCategories } from './services/api';
+
+export default class Home extends Component {
+  render() {
+    console.log(getCategories());
+    return (
+      <h1>Home</h1>
+    );
+  }
+}
--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,25 @@
 export async function getCategories() {
-  // Implemente aqui
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const data = await response.json();
+  return data;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  if (categoryId) {
+    const response = await fetch(` https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
+    const dataTest = await response.json();
+    return dataTest;
+  }
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`);
+  const data = await response.json();
+  return data;
+}
+
+export async function getProductsFromCategory(categoryId) {
+  const URL = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
+  const response = await fetch(URL);
+  const data = await response.json();
+  return data;
 }
 
 export async function getProductById() {
