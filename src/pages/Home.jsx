39e89acf70129ce1aaebffe664c7d01ed7ceef4a
Reb1324 <105323507+Reb1324@users.noreply.github.com>
2023-02-09 21:28:25 -0300

    Requisito 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { getCategories } from '../services/api';
+import Search from '../components/Search';
 
 export default class Home extends Component {
   render() {
     console.log(getCategories());
     return (
-      <h1>Home</h1>
+      <Search />
     );
   }
 }
