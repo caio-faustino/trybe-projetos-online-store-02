4daa8006d8ed375a87dba6d50faa7cb2f16ad2fc
Reb1324 <105323507+Reb1324@users.noreply.github.com>
2023-02-22 19:17:35 -0300

    fixing req007

--- src/components/ProductQuery.jsx
+++ src/components/ProductQuery.jsx
@@ -5,13 +5,16 @@ class productQuery extends Component {
   render() {
     const { image, price, title } = this.props;
     return (
-      <section data-testid="product">
-        <section>
-          <h2>{title}</h2>
-          <img src={ image } alt={ `imagem de um ${title}` } />
-          <h2>{price}</h2>
+      <Link
+        data-testid="product-detail-link"
+        to={ `/Details/${id}` }
+      >
+        <section data-testid="product">
+            <h2>{title}</h2>
+            <img src={ image } alt={ `imagem de um ${title}` } />
+            <h2>{price}</h2>
         </section>
-      </section>
+      </Link>
     );
   }
 }
