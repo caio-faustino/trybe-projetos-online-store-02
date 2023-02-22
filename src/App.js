bdc3154a30173e8b9791a90140a823c930d1a7f5
Reb1324 <105323507+Reb1324@users.noreply.github.com>
2023-02-22 02:16:08 -0300

    requisito 7

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
+import Details from './pages/Details';
 
 export default class App extends Component {
   render() {
@@ -10,6 +11,7 @@ export default class App extends Component {
         <Switch>
           <Route exact path="/" component={ Home } />
           <Route path="/cart" component={ Cart } />
+          <Route exact path="/details/:id" component={ Details } />
         </Switch>
       </BrowserRouter>
     );
---
+++ src/pages/Details.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Details extends Component {
+  state = {
+    price: 0,
+    image: '',
+    title: '',
+  };
+
+  componentDidMount() {
+    this.getItem();
+  }
+
+  getItem = async () => {
+    const { match: { params: { id } } } = this.props;
+    this.fetchProduct(id);
+  };
+
+  fetchProduct = async (productId) => {
+    const response = await fetch(`https://api.mercadolibre.com/items/${productId}`);
+    const data = await response.json();
+
+    this.setState({
+      price: data.price,
+      image: data.thumbnail,
+      title: data.title,
+    });
+  };
+
+  render() {
+    const {
+      price,
+      image,
+      title,
+    } = this.state;
+
+    return (
+      <section>
+        <div>
+          <h2 data-testid="product-detail-name" >{title}</h2>
+          <img data-testid="product-detail-image" src={ image } alt={ `imagem de um ${title}` } />
+          <h2 data-testid="product-detail-price" >{price}</h2>
+        </div>
+      </section>
+    );
+  }
+}
+
+Details.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }),
+}.isRequired;
